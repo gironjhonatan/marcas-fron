79676f77d0eb1073726347ad0d386b4ff47417ae
JHONATAN <gironjhonatan85@gmail.com>
2025-08-21 21:43:08 -0500

    repository

--- app/api/authAPI.js
+++ app/api/authAPI.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8000/auth";
+const API_URL = "http://159.89.157.13:8000/auth";
 
 export async function registerUser({ email, password }) {
   const res = await fetch(`${API_URL}/register`, {
